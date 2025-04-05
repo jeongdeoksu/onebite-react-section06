13ce21b898435c0cbec916a64da6e9954f8db04f
Deoksu Jeong <jeongdeoksu@gmail.com>
2025-04-05 09:02:47 +0900

    Completed Counter app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import Controller from './components/Controller';
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(0);
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
 
   return (
     <div className="App">
@@ -13,7 +17,7 @@ function App() {
         <Viewer count={count} />
       </section>
       <section>
-        <Controller />
+        <Controller onClickButton={onClickButton} />
       </section>
     </div>
   );
